1531bbce1d9598873e95a1c3e89fd2dc461a030f
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-18 00:42:14 +0300

    feat: create validate_password and validate_email function in database module

---
+++ src/database/validators/accounts.py
@@ -0,0 +1,33 @@
+import re
+
+import email_validator
+from email_validator import EmailNotValidError
+
+
+def validate_password_strength(password: str) -> str:
+    if len(password) < 8:
+        raise ValueError("Password must contain at least 8 characters.")
+    elif re.search("r[A-Z]", password):
+        raise ValueError("Password must contain at least one uppercase letter.")
+    elif re.search("r[a-z]", password):
+        raise ValueError("Password must contain at least one lower letter.")
+    elif re.search("r[0-9]", password):
+        raise ValueError("Password must contain at least one digit.")
+    elif re.search("r[@$!%*?&#]", password):
+        raise ValueError(
+            "Password must contain at least one special character: @, $, !, %, *, ?, &, #."
+        )
+
+    return password
+
+
+def validate_email(user_email: str) -> str:
+    try:
+        email_info = email_validator.validate_email(
+            user_email, check_deliverability=False
+        )
+        email = email_info.normalized
+    except EmailNotValidError as e:
+        raise ValueError(str(e))
+    else:
+        return email

3b47acce740f0e78739d3049bc48bebba7a9a251
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-17 13:54:43 +0300

    feat: create get_jwt_manager dependency

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,6 +1,10 @@
 import os
 
+from fastapi import Depends
+
 from config.settings import Settings, DevelopmentSettings, BaseAppSettings
+from security.interfaces import JWTManagerInterface
+from security.manager import JWTManager
 
 
 def get_settings() -> BaseAppSettings:
@@ -8,3 +12,15 @@ def get_settings() -> BaseAppSettings:
     if environment == "production":
         return Settings()
     return DevelopmentSettings()
+
+
+def get_jwt_manager(
+    settings: BaseAppSettings = Depends(get_settings)
+) -> JWTManagerInterface:
+    return JWTManager(
+        access_secret_key=settings.SECRET_KEY_ACCESS,
+        refresh_secret_key=settings.SECRET_KEY_REFRESH,
+        access_expires_delta=settings.ACCESS_TOKEN_EXPIRE_MINUTES,
+        refresh_expires_delta=settings.REFRESH_TOKEN_EXPIRE_MINUTES,
+        algorithm=settings.JWT_SIGNING_ALGORITHM
+    )

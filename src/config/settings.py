e32305c6619b782538c6fb033d4b4e41905064ef
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-17 12:54:38 +0300

    feat: add attributes in settings for jwt

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from pydantic_settings import BaseSettings
@@ -6,6 +7,16 @@ from pydantic_settings import BaseSettings
 class BaseAppSettings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "cinema.db")
+
+    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
+    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
+    JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)
+    )
+    REFRESH_TOKEN_EXPIRE_MINUTES: int = int(
+        os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 24 * 60)
+    )
 
 
 class Settings(BaseAppSettings):

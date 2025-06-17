8a24d73e78555507d89ff9f26f41498c3a2e3ec0
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-17 15:42:11 +0300

    feat: create get_token dependency

--- src/config/dependencies.py
+++ src/config/dependencies.py
@@ -1,6 +1,6 @@
 import os
 
-from fastapi import Depends
+from fastapi import Depends, Request, HTTPException, status
 
 from config.settings import Settings, DevelopmentSettings, BaseAppSettings
 from security.interfaces import JWTManagerInterface
@@ -24,3 +24,23 @@ def get_jwt_manager(
         refresh_expires_delta=settings.REFRESH_TOKEN_EXPIRE_MINUTES,
         algorithm=settings.JWT_SIGNING_ALGORITHM
     )
+
+
+def get_token(request: Request) -> str:
+    authorization: str = request.headers.get("Authorization")
+
+    if not authorization:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Authorization header is missing."
+        )
+
+    scheme, _, token = authorization.partition(" ")
+
+    if scheme.lower() != "bearer" or not token:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid Authorization header format. Expected 'Bearer <token>'"
+        )
+
+    return token

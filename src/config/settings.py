44b6ad9aa214c9f0c3705795483c3eaad21ced9e
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-17 22:34:36 +0300

    feat: add attributes in settings for email

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+from pydantic import EmailStr, SecretStr
 from pydantic_settings import BaseSettings
 
 
@@ -17,6 +18,16 @@ class BaseAppSettings(BaseSettings):
     REFRESH_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("REFRESH_TOKEN_EXPIRE_MINUTES", 24 * 60)
     )
+
+    EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "notifications" / "templates")
+    MAIL_SERVER: str = os.getenv("MAIL_SERVER", "mailhog")
+    MAIL_PORT: int = int(os.getenv("MAIL_PORT", 25))
+    MAIL_USERNAME: str = os.getenv("MAIL_USERNAME", "test_user")
+    MAIL_PASSWORD: SecretStr = os.getenv("MAIL_PASSWORD", "some_password")
+    MAIL_FROM: EmailStr = os.getenv("MAIL_FROM", "test@email.com")
+    MAIL_FROM_NAME: str = os.getenv("MAIL_FROM_NAME", "Test User")
+    MAIL_STARTTLS: bool = os.getenv("MAIL_STARTTLS", False).lower() == "true"
+    MAIL_SSL_TLS: bool = os.getenv("MAIL_SSL_TLS", False).lower() == "true"
 
 
 class Settings(BaseAppSettings):

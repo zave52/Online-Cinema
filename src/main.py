9bf1fea7f80cd40d6a2bce3e435c9a3238c4d66a
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-18 21:33:24 +0300

    feat: include accounts router in main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 
+from routers.accounts import router
+
 app = FastAPI(title="Online Cinema")
 
 api_version_index = "/api/v1"
+
+app.include_router(
+    router, prefix=f"{api_version_index}/accounts", tags=["accounts"]
+)

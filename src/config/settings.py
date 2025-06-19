7ffdedfd6ffb3aabddfef49765914342c8a2424a
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-19 18:35:33 +0300

    fix: fix incorrect type for default value in SECRET_KEY_ACCESS and SECRET_KEY_REFRESH

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,8 +10,14 @@ class BaseAppSettings(BaseSettings):
     BASE_DIR: Path = Path(__file__).parent.parent
     PATH_TO_DB: str = str(BASE_DIR / "database" / "source" / "cinema.db")
 
-    SECRET_KEY_ACCESS: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
-    SECRET_KEY_REFRESH: str = os.getenv("SECRET_KEY_ACCESS", os.urandom(32))
+    SECRET_KEY_ACCESS: str = os.getenv(
+        "SECRET_KEY_ACCESS",
+        str(os.urandom(32))
+    )
+    SECRET_KEY_REFRESH: str = os.getenv(
+        "SECRET_KEY_ACCESS",
+        str(os.urandom(32))
+    )
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")
     ACCESS_TOKEN_EXPIRE_MINUTES: int = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30)

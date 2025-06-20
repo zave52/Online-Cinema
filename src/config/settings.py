bd9719fd6f42474c047b3b9a1ccb83af33d32a6e
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-20 08:15:55 +0300

    feat: add setting variables for S3 storage

--- src/config/settings.py
+++ src/config/settings.py
@@ -36,6 +36,16 @@ class BaseAppSettings(BaseSettings):
     MAIL_STARTTLS: bool = os.getenv("MAIL_STARTTLS", "False").lower() == "true"
     MAIL_SSL_TLS: bool = os.getenv("MAIL_SSL_TLS", "False").lower() == "true"
 
+    S3_STORAGE_HOST: str = os.getenv("MINIO_HOST", "minio-cinema")
+    S3_STORAGE_PORT: int = os.getenv("MINIO_PORT", 9000)
+    S3_STORAGE_ACCESS_KEY: str = os.getenv("MINIO_ROOT_USER", "test_user")
+    S3_STORAGE_SECRET_KEY: str = os.getenv("MINIO_ROOT_PASSWORD", "password")
+    S3_BUCKET_NAME: str = os.getenv("MINIO_STORAGE", "online-cinema-storage")
+
+    @property
+    def S3_STORAGE_ENDPOINT(self) -> str:
+        return f"http://{self.S3_STORAGE_HOST}:{self.S3_STORAGE_PORT}"
+
 
 class Settings(BaseAppSettings):
     pass

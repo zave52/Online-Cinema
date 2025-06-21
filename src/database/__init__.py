f74cf14baa89586df80f24b8b2d0a4f17b4c7ab3
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-21 19:24:39 +0300

    feat: add import AsyncSessionLocal for __init__.py in database module

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -5,4 +5,7 @@ environment = os.getenv("ENVIRONMENT", "developing")
 if environment == "production":
     pass
 else:
-    from session_sqlite import get_sqlite_db as get_db
+    from .session_sqlite import (
+        get_sqlite_db as get_db,
+        AsyncSQLiteSessionLocal as AsyncSessionLocal
+    )

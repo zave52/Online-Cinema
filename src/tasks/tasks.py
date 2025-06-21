9a3ac3c5512bbd9641bb8031ddbb9c0c4ad34fbc
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-21 19:38:55 +0300

    feat: add delete_expires_password_reset_tokens and delete_expires_refresh_tokens periodic tasks

--- src/tasks/tasks.py
+++ src/tasks/tasks.py
@@ -5,7 +5,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from .celery_app import async_task
 from database import AsyncSessionLocal
-from database.models.accounts import ActivationTokenModel
+from database.models.accounts import (
+    ActivationTokenModel,
+    PasswordResetTokenModel,
+    RefreshTokenModel
+)
 
 
 @async_task()
@@ -15,6 +19,34 @@ async def delete_expires_activation_tokens() -> None:
         stmt = (
             delete(ActivationTokenModel)
             .where(ActivationTokenModel.expires_at < datetime.now(timezone.utc))
+            .execution_options(synchronize_session=False)
+        )
+        await session.execute(stmt)
+        await session.commit()
+
+
+@async_task()
+async def delete_expires_password_reset_tokens() -> None:
+    async with AsyncSessionLocal() as session:
+        session: AsyncSession
+        stmt = (
+            delete(PasswordResetTokenModel)
+            .where(
+                PasswordResetTokenModel.expires_at < datetime.now(timezone.utc)
+            )
+            .execution_options(synchronize_session=False)
+        )
+        await session.execute(stmt)
+        await session.commit()
+
+
+@async_task()
+async def delete_expires_refresh_tokens() -> None:
+    async with AsyncSessionLocal() as session:
+        session: AsyncSession
+        stmt = (
+            delete(RefreshTokenModel)
+            .where(RefreshTokenModel.expires_at < datetime.now(timezone.utc))
             .execution_options(synchronize_session=False)
         )
         await session.execute(stmt)

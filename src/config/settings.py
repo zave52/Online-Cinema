2ec297c6cb1c3eb375351bbb4e1cbdb4bd590e08
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-21 16:19:47 +0300

    fix: wrong environment variable for SECRET_KEY_REFRESH variable in settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -15,7 +15,7 @@ class BaseAppSettings(BaseSettings):
         str(os.urandom(32))
     )
     SECRET_KEY_REFRESH: str = os.getenv(
-        "SECRET_KEY_ACCESS",
+        "SECRET_KEY_REFRESH",
         str(os.urandom(32))
     )
     JWT_SIGNING_ALGORITHM: str = os.getenv("JWT_SIGNING_ALGORITHM", "HS256")

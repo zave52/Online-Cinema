def51fbca5930cdaedfc4b10da1581baf3c534f0
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-24 11:29:12 +0300

    feat: add max_lenght constraing to name field in Genre, Star, Director and Certification schema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,30 +1,30 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class GenreSchema(BaseModel):
     id: int
-    name: str
+    name: str = Field(..., max_length=100)
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class StarSchema(BaseModel):
     id: int
-    name: str
+    name: str = Field(..., max_length=100)
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class DirectorSchema(BaseModel):
     id: int
-    name: str
+    name: str = Field(..., max_length=100)
 
     model_config = ConfigDict(from_attributes=True)
 
 
 class CertificationSchema(BaseModel):
     id: int
-    name: str
+    name: str = Field(..., max_length=100)
 
     model_config = ConfigDict(from_attributes=True)
 

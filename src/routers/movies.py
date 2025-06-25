5e0f7ea628c2825985883fc7f06b26b0af851d9f
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-25 09:59:37 +0300

    feat: add create_movie endpoint

--- src/routers/movies.py
+++ src/routers/movies.py
@@ -1,19 +1,27 @@
 from typing import Sequence, Optional
 
-from fastapi import APIRouter, status, Depends, Query
+from fastapi import APIRouter, status, Depends, Query, HTTPException
 from sqlalchemy import select, func, or_, desc, asc
+from sqlalchemy.exc import SQLAlchemyError, IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import selectinload
 
+from config.dependencies import get_token, get_jwt_manager
 from database import get_db
+from database.models.accounts import UserModel, UserGroupModel, UserGroupEnum
 
 from database.models.movies import (
     MovieModel,
     StarModel,
     DirectorModel,
-    GenreModel
+    GenreModel, CertificationModel
 )
-from schemas.movies import MovieListResponseSchema, MovieListItemSchema
+from exceptions.security import BaseSecurityError
+from schemas.movies import (
+    MovieListResponseSchema,
+    MovieListItemSchema, MovieCreateResponseSchema, MovieCreateRequestSchema
+)
+from security.interfaces import JWTManagerInterface
 
 router = APIRouter()
 
@@ -99,3 +107,144 @@ async def get_movies(
         total_pages=total_pages,
         total_items=total_items
     )
+
+
+@router.post(
+    "/movies/",
+    response_model=MovieCreateResponseSchema,
+    status_code=status.HTTP_201_CREATED,
+    tags=["admin", "moderator"]
+)
+async def create_movie(
+    data: MovieCreateRequestSchema,
+    token: str = Depends(get_token),
+    jwt_manager: JWTManagerInterface = Depends(get_jwt_manager),
+    db: AsyncSession = Depends(get_db)
+) -> MovieCreateResponseSchema:
+    try:
+        decoded_token = jwt_manager.decode_access_token(token)
+        user_id = decoded_token.get("user_id")
+    except BaseSecurityError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=str(e)
+        )
+
+    existing_stmt = (
+        select(MovieModel)
+        .where(
+            MovieModel.name == data.name,
+            MovieModel.year == data.year,
+            MovieModel.time == data.time
+        )
+    )
+    result = await db.execute(existing_stmt)
+    existing_movie: MovieModel = result.scalars().first()
+
+    if existing_movie:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"A movie with the name '{existing_movie.name}', release year "
+                   f"'{existing_movie}' and time duration '{existing_movie.time}' "
+                   f"already exists."
+        )
+
+    group_stmt = (
+        select(UserGroupModel)
+        .join(UserModel)
+        .where(UserModel.id == user_id)
+    )
+    result = await db.execute(group_stmt)
+    user_group = result.scalars().first()
+
+    if not user_group or user_group not in (
+            UserGroupEnum.MODERATOR, UserGroupEnum.ADMIN
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Only administrators and moderators can create movie."
+        )
+
+    try:
+        certification_stmt = (
+            select(CertificationModel)
+            .where(CertificationModel.name == data.certification)
+        )
+        result = await db.execute(certification_stmt)
+        certification = result.scalars().first()
+
+        if not certification:
+            certification = CertificationModel(name=data.certification)
+            db.add(certification)
+            await db.flush()
+
+        genres = []
+        for genre_name in data.genres:
+            genre_stmt = select(GenreModel).where(GenreModel.name == genre_name)
+            result = await db.execute(genre_stmt)
+            genre = result.scalars().first()
+
+            if not genre:
+                genre = GenreModel(name=genre_name)
+                db.add(genre)
+                await db.flush()
+
+            genres.append(genre)
+
+        stars = []
+        for star_name in data.stars:
+            star_stmt = select(StarModel).where(StarModel.name == star_name)
+            result = await db.execute(star_stmt)
+            star = result.scalars().first()
+
+            if not star:
+                star = StarModel(name=star_name)
+                db.add(star)
+                await db.flush()
+
+            stars.append(star)
+
+        directors = []
+        for director_name in data.directors:
+            director_stmt = select(DirectorModel).where(
+                DirectorModel.name == director_name
+            )
+            result = await db.execute(director_stmt)
+            director = result.scalars().first()
+
+            if not director:
+                director = DirectorModel(name=director_name)
+                db.add(director)
+                await db.flush()
+
+            directors.append(director)
+
+        movie = MovieModel(
+            name=data.name,
+            year=data.year,
+            time=data.time,
+            imdb=data.imdb,
+            votes=data.votes,
+            meta_score=data.meta_score,
+            gross=data.gross,
+            description=data.description,
+            price=data.price,
+            certification=certification,
+            genres=genres,
+            stars=stars,
+            directors=directors
+        )
+        db.add(movie)
+        await db.commit()
+        await db.refresh(
+            movie,
+            ["certification", "genres", "stars", "directors"]
+        )
+    except IntegrityError:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Invalid input data."
+        )
+
+    return MovieCreateResponseSchema.model_validate(movie)

e103e26b69aa8fe68842e3dcbbf8b5e2db4d9ca5
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-27 14:02:16 +0300

    feat: add created_at field in CommentSchema

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,5 +1,6 @@
 from typing import List, Any, Optional
 from uuid import UUID
+from datetime import datetime
 
 from pydantic import BaseModel, ConfigDict, Field, field_validator
 
@@ -60,6 +61,7 @@ class CertificationSchema(BaseModel):
 class CommentSchema(BaseModel):
     id: int
     content: str
+    created_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
 

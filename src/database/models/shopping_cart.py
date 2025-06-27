9874e3e793566a94db11b6634fb2a748d0c0eee9
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-27 18:15:36 +0300

    feat: add Cart and CartItem models

---
+++ src/database/models/shopping_cart.py
@@ -0,0 +1,75 @@
+from datetime import datetime, timezone
+from typing import List
+
+from sqlalchemy import Integer, ForeignKey, DateTime, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.models.accounts import UserModel
+from database.models.base import Base
+from database.models.movies import MovieModel
+
+
+class CartModel(Base):
+    __tablename__ = "carts"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        unique=True
+    )
+
+    user: Mapped[UserModel] = relationship(
+        UserModel,
+        backref="cart"
+    )
+    items: Mapped[List["CartItemModel"]] = relationship(
+        "CartItemModel",
+        back_populates="cart"
+    )
+
+    def __repr__(self) -> str:
+        return f"<CartModel(id={self.id}, user_id={self.user_id})>"
+
+
+class CartItemModel(Base):
+    __tablename__ = "cart_items"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+    added_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.now(timezone.utc)
+    )
+
+    cart_id: Mapped[int] = mapped_column(
+        ForeignKey("carts.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    movie_id: Mapped[int] = mapped_column(
+        ForeignKey("movies.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+    cart: Mapped[CartModel] = relationship(
+        CartModel,
+        back_populates="items"
+    )
+    movie: Mapped[MovieModel] = relationship(
+        MovieModel,
+        backref="cart_items"
+    )
+
+    __table_args__ = (UniqueConstraint("cart_id", "movie_id"),)
+
+    def __repr__(self) -> str:
+        return (f"<CartItemModel(id={self.id}, movie_id={self.movie_id}, "
+                f"cart_id={self.cart_id}, added_at={str(self.added_at)})>")

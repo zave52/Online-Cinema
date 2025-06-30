c85d3f7a4cfc3568b5e376526f2e988c5a6be29d
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-30 15:06:48 +0300

    feat: add payments exceptions

---
+++ src/exceptions/payments.py
@@ -0,0 +1,14 @@
+class PaymentError(Exception):
+    pass
+
+
+class WebhookError(Exception):
+    pass
+
+
+class PaymentValidationError(Exception):
+    pass
+
+
+class PaymentProcessingError(Exception):
+    pass 
